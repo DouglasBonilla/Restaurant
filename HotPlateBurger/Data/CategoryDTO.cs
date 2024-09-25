6312dd66b47cfe8b3f03d9641ef8c155db1a67d5
Victort503j <victor.romerorc@gmail.com>
2024-09-25 15:54:19 -0600

    Proyecto migrado a .net 8

--- HotPlateBurger/Data/CategoryDTO.cs
+++ HotPlateBurger/Data/CategoryDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace HotPlateBurger.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotPlateBurger.Data
 {
     public class CategoryDTO
     {
+        public int Id { get; set; }
+
+        public String Nombre { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }
     }
 }
+
+
--- HotPlateBurger/Program.cs
+++ HotPlateBurger/Program.cs
@@ -7,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7264/api/")
+});
 
 var app = builder.Build();
 
