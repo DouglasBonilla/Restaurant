a38fb5f4658edd929c98ba036e9e1b230cdf4854
Victort503j <victor.romerorc@gmail.com>
2024-09-26 23:28:24 -0600

    cambios de diseño en categoria y usuario

--- HotPlateBurger/Data/UserDTO.cs
+++ HotPlateBurger/Data/UserDTO.cs
@@ -2,5 +2,11 @@
 {
     public class UserDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
