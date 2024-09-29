728a4706649a7137778a0ec7cc266184bcdf4fe7
Henry25t <henrycarias96@gmail.com>
2024-09-29 12:36:10 -0600

    Funcionalidada de guardar food con imagenes

--- HotPlateBurger/Data/FoodDTO.cs
+++ HotPlateBurger/Data/FoodDTO.cs
@@ -2,5 +2,12 @@
 {
     public class FoodDTO
     {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Price { get; set; }
+        public string Picture { get; set; }
+
     }
 }
